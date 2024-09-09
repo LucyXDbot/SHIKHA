a20a730c5ffe5ec94c6e9be356b356974b2c4f61
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-09 10:02:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KiritoxAsuna02/SHIKHA",
+    "https://github.com/LucyXDbot/SHIKHA",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
